9bac7c49098e13074d75bd867c4fd544b7e12e16
Singh-csm <csm8107@gmail.com>
2023-04-05 18:26:53 +0530

    controller update

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,7 +2,7 @@ import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) =>{
    try {
-        const postMessages = await postMessage.find();
+        const postMessages = await PostMessage.find();
 
         res.status(200).json(postMessages);
 
