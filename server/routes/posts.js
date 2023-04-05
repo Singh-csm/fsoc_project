ac0505ededfc4714de4db0bdb3c22cbb2ab46397
Singh-csm <csm8107@gmail.com>
2023-04-05 18:18:23 +0530

    controller, model, route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
  import express from 'express';
 
+ import { getPosts, createPost } from '../controllers/posts.js';
+
  const router = express.Router();
 
- router.get('/', (req, res) => {
-   res.send('Hello World');
- });
+ router.get('/', getPosts);
+ router.post("/", createPost);
 
  export default router;
